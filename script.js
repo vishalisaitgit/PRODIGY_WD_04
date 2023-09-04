fd9e3a059161008ab86d0288bd29a591b71c279e
vishalisaitgit <115639913+vishalisaitgit@users.noreply.github.com>
2023-09-04 18:17:54 +0530

    script.js

--- script.js
+++ script.js
@@ -36,19 +36,19 @@ async function checkWeather(city) {
 
   switch (weather_data.weather[0].main) {
     case "Clouds":
-      weather_img.src = "cloud.png";
+      weather_img.src = "images/cloud.png";
       break;
     case "Clear":
-      weather_img.src = "clear.png";
+      weather_img.src = "images/clear.png";
       break;
     case "Rain":
-      weather_img.src = "rain.png";
+      weather_img.src = "images/rain.png";
       break;
     case "Mist":
-      weather_img.src = "mist.png";
+      weather_img.src = "images/mist.png";
       break;
     case "Snow":
-      weather_img.src = "snow.png";
+      weather_img.src = "images/snow.png";
       break;
   }
 
